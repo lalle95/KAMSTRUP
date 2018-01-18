23cfc46f5d5b23014d7a4bb8f5ad3556415afb7d
Lasse Lildholdt <LLI@kamstrup.dk>
2018-01-18 09:06:46 +0100

    Uploading new jenkinsfile with belonging unit test

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -10,6 +10,7 @@ pipeline {
         stage('Test') {
             steps {
                 echo 'Testing..'
+                def cmd = python test_calculator.py
             }
         }
         stage('Deploy') {
---
+++ test_calculator.py
@@ -0,0 +1,29 @@
+import unittest
+import calculator
+
+
+class TestCalculator(unittest.TestCase):
+
+    def test_add(self):
+        self.assertEqual(calculator.add(10, 5), 15)
+        self.assertEqual(calculator.add(-2, -2), -4)
+        self.assertEqual(calculator.add(-2, 2), 0)
+        self.assertEqual(calculator.add(2, 2), 4)
+
+    def test_subtract(self):
+        self.assertEqual(calculator.subtract(10, 5), 5)
+
+    def test_multiply(self):
+        self.assertEqual(calculator.multiply(10, 5), 50)
+
+    def test_divide(self):
+        self.assertEqual(calculator.divide(10, 5), 2)
+        with self.assertRaises(ValueError):
+            calculator.divide(10, 0)
+
+    def test_formatter(self):
+        self.assertIsInstance(calculator.formatter(5), str)
+
+
+if __name__ == '__main__':
+    unittest.main()
