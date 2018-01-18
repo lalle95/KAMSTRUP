00a78645be10e24d5f3c6065c1b49b264b39fc9d
Lasse Lildholdt <LLI@kamstrup.dk>
2018-01-18 11:03:28 +0100

    New jenkinsfile for developemtn branch

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,52 +1,21 @@
 pipeline {
-  agent any
-  stages {
-    stage('build') {
-      parallel {
-        stage('print') {
-          steps {
-            echo 'building ...'
-          }
+    agent any
+
+    stages {
+        stage('Build') {
+            steps {
+                echo 'Building..'
+            }
         }
-        stage('print parallel') {
-          steps {
-            echo 'building in parallel ...'
-          }
+        stage('Test') {
+            steps {
+                bat 'python test_calculator.py'
+            }
         }
-      }
+        stage('Deploy') {
+            steps {
+                echo 'Deploying....'
+            }
+        }
     }
-    stage('test') {
-      parallel {
-        stage('print') {
-          steps {
-            echo 'testing ... '
-          }
-        }
-        stage('print parallel') {
-          steps {
-            echo 'testing in parallel ...'
-          }
-        }
-        stage('Unit test') {
-          steps {
-            bat 'python test_calculator.py'
-          }
-        }
-      }
-    }
-    stage('deploy') {
-      parallel {
-        stage('print') {
-          steps {
-            echo 'deploying ...'
-          }
-        }
-        stage('print parallel') {
-          steps {
-            echo 'deploying in parallel ...'
-          }
-        }
-      }
-    }
-  }
 }
