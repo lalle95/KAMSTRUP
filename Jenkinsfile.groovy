5851afbc57338fa895126e3652d097d7ee39e166
Lasse Lildholdt <LLI@kamstrup.com>
2018-01-19 10:42:07 +0100

    Uploading new jenkinsfile handling busybox as docker images for test stage

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,20 +2,28 @@ pipeline {
     agent any
 
     stages {
+
         stage('Build') {
             steps {
                 echo 'Building..'
             }
         }
+
         stage('Test') {
+            agent {
+                docker { image 'busybox' }
+            }
             steps {
                 echo 'Testing..'
             }
         }
+        
         stage('Deploy') {
             steps {
                 echo 'Deploying....'
             }
+
         }
     }
 }
+
